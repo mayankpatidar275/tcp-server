61ad613d61b7144caf90ebc472ef7ad0a13f6ebf
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2024-12-12 06:44:03 +0530

    feat: read the and write (continously)
    Step 3: Read the request data, Write and send the response, Close the connection.
    Step 4: Do it over and over again(for loop)

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
+	"time"
 )
+
+func do(conn net.Conn){
+	buf := make([]byte, 1024)
+	// We can extract the http url and write the further logic for methods and all, just like other servers like flask and all does 
+	_,err := conn.Read(buf);
+	if err!=nil {
+		log.Fatal("Error reading: ", err);
+	}
+
+	time.Sleep(1*time.Second)
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, world!\r\n"));
+
+	conn.Close();
+}
 
 // Creating a TCP server from scratch
 func main(){
@@ -16,11 +31,21 @@ func main(){
 	}
 
 	// Step 2: Wait for client to connect. 'Accept' is a blocking system call
-	conn, err := listener.Accept();
-	if err!=nil {
-		log.Fatal("Error accepting: ", err);
-	}
+	// conn, err := listener.Accept();
+	// if err!=nil {
+	// 	log.Fatal("Error accepting: ", err);
+	// }
 
-	fmt.Println(conn)
+	// Step 3: Read the request data, Write and send the response, Close the connection
+	// do(conn)
+
+	// Step 4: Do it over and over again
+	for{
+		conn, err := listener.Accept();
+		if err!=nil {
+			log.Fatal("Error accepting: ", err);
+		}
+		do(conn)
+	}
 
 }
